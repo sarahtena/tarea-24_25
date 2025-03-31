9f909f8edf3a075ed8f6ef3bff87402d1365edb0
Sarah Tena <sarahtena2006@gmail.com>
2025-03-31 11:17:03 +0200

    metodos de entity

---
+++ Entity.py
@@ -0,0 +1,14 @@
+class Entity:
+    def __init__(self, x, y, image):
+        self.x = x
+        self.y = y
+        self.image = image
+    def __str__(self):
+        return 
+
+    def move(self, dx, dy):
+        self.x += dx
+        self.y += dy
+
+    def draw(self):
+        print(f"Drawing entity at ({self.x}, {self.y}) with image {self.image}")
